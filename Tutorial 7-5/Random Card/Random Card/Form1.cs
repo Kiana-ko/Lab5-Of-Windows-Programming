af69cdc088fb37ac36b21177d28e285b8f5acd92
Kiana <94536969+Kiana-ko@users.noreply.github.com>
2023-06-08 12:52:22 -0700

    Add files via upload

---
+++ Tutorial 7-5/Random Card/Random Card/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Random_Card
+{
+    public partial class Form1 : Form
+    {
+        private class Card
+        {
+            public string Value { get; set; }
+            public string Suit { get; set; }
+        }
+
+        private Card[] cardArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCardArray();
+        }
+
+        private void InitializeCardArray()
+        {
+            // Populate the cardArray with card objects
+            cardArray = new Card[]
+            {
+                new Card { Value = "Ace", Suit = "Hearts" },
+                new Card { Value = "2", Suit = "Hearts" },
+                // Add more card objects as needed
+            };
+        }
+
+        private void getCardButton_Click(object sender, EventArgs e)
+        {
+            // Create a Random object.
+            Random randomCard = new Random();
+
+            // Get a random index.
+            int randomCardIndex = randomCard.Next(0, cardArray.Length);
+
+            // Display a card.
+            Card randomlyPickedCard = cardArray[randomCardIndex];
+            MessageBox.Show($"{randomlyPickedCard.Value} of {randomlyPickedCard.Suit}");
+        }
+
+
+        private void cardPictureBox_Click(object sender, EventArgs e)
+        {
+            // Handle the click event for the picture box if needed
+        }
+    }
+}
---
+++ Tutorial 8-2/Telephone Format/Telephone Format/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Telephone_Format
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The IsValidNumber method accepts a string and
+        // returns true if it contains 10 digits, or false
+        // otherwise.
+        private bool IsValidNumber(string str)
+        {
+            const int VALID_LENGTH = 10;  // Length of a valid string
+            bool validNumber = true;   // Flag to indicate validity
+
+            if (str.Length != VALID_LENGTH)
+            {
+                validNumber = false;
+            }
+            else
+            {
+                // Check each character in str.
+                for (int i = 0; i < str.Length; i++)
+                {
+                    // If this character is not a digit, the string is not valid.
+                    int digitNumbers;
+                    if (!int.TryParse(str[i].ToString(), out digitNumbers))
+                    {
+                        validNumber = false;
+                        break;
+                    }
+                }
+            }
+
+            return validNumber;
+        }
+
+
+        // The TelephoneFormat method accepts a string argument
+        // by reference and formats it as a telephone number.
+        private void TelephoneFormat(ref string str)
+        {
+            // First, insert the left paren at position 0.
+            str = str.Insert(0, "(");
+
+            // Next, insert the right paren at position 4.
+            str = str.Insert(4, ")");
+
+            // Next, insert the hyphen at position 8.
+            str = str.Insert(8, "-");
+        }
+
+        private void formatButton_Click(object sender, EventArgs e)
+        {
+            // Get a trimmed copy of the user's input.
+            string input = numberTextBox.Text.Trim();
+
+            // If the input is a valid number, format it
+            // and display it.
+            if (IsValidNumber(input))
+            {
+                TelephoneFormat(ref input);
+                MessageBox.Show(input);
+            }
+            else
+            {
+                // Display an error message.
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
